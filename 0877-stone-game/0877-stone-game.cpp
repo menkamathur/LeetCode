5b3790b314f8b387777d8f0ae5b34dfd0da13991
Menka <149295516+menkamathur@users.noreply.github.com>
2026-08-02 22:14:35 +0530

    Time: 7 ms (35.23%), Space: 11.6 MB (36.14%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -9,7 +9,7 @@ public:
 
         for (int size = 1; size <= N; ++size)
             for (int i = 0, j = size - 1; j < N; ++i, ++j) {
-                int parity = (j + i + N) % 2;  // j - i - N; but +x = -x (mod 2)
+                int parity = (j + i + N) % 2;  
                 if (parity == 1)
                     dp[i+1][j+1] = max(piles[i] + dp[i+2][j+1], piles[j] + dp[i+1][j]);
                 else
