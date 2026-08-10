f8a55a25a1f1becfea975df6a3b691658b26b7c6
Menka <149295516+menkamathur@users.noreply.github.com>
2026-08-10 22:17:01 +0530

    Time: 31 ms (81.44%), Space: 9 MB (82.55%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+   
+    bool winnerSquareGame(int n) {
+        vector<bool> t(n+1, false);
+
+        // Base case . n == 0, return false
+        // i = 0
+        t[0] = false; 
+        for(int i = 0; i < n+1; i++){
+            for(int k = 1; k*k <= i; k++){
+                if(t[i - (k*k)] == false){
+                    t[i] = true;
+                    break;
+                }
+            }
+        }
+        return t[n];
+    }
+};
